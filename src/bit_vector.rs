72f094642255bb96e9e6c2bb1f718f113416f898
Thomas Pendock <tgpendock@gmail.com>
2025-07-21 22:49:09 -0700

    Change atomic feature to be two structs instead: BloomFilter and AtomicBloomFilter

--- src/bit_vector.rs
+++ src/bit_vector.rs
@@ -1,5 +1,5 @@
+use crate::AtomicU64;
 use alloc::{boxed::Box, vec::Vec};
-#[cfg(feature = "atomic")]
 use core::sync::atomic::Ordering::Relaxed;
 
 /// The number of bits in the bit mask that is used to index a u64's bits.
@@ -10,70 +10,92 @@ const BIT_MASK_LEN: u32 = u32::ilog2(u64::BITS);
 /// Gets 6 last bits from the bit index, which are used to index a u64's bits.
 const BIT_MASK: u64 = (1 << BIT_MASK_LEN) - 1;
 
-use crate::number::*;
+/// A bit vector partitioned in to `u64` blocks.
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub(crate) struct BitVec {
+    bits: Box<[u64]>,
+}
 
 /// A bit vector partitioned in to `u64` blocks.
 #[derive(Debug)]
-#[cfg_attr(not(feature = "atomic"), derive(Clone))]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub(crate) struct BlockedBitVec {
-    bits: Box<[BitStorage]>,
+pub(crate) struct AtomicBitVec {
+    bits: Box<[AtomicU64]>,
 }
 
-impl BlockedBitVec {
-    #[inline]
-    pub(crate) const fn len(&self) -> usize {
-        self.bits.len()
-    }
+macro_rules! impl_bitvec {
+    ($name:ident, $bits:ty) => {
+        impl $name {
+            #[inline]
+            pub(crate) const fn len(&self) -> usize {
+                self.bits.len()
+            }
 
-    #[inline]
-    pub(crate) const fn num_bits(&self) -> usize {
-        self.len() * u64::BITS as usize
+            #[inline]
+            pub(crate) const fn num_bits(&self) -> usize {
+                self.len() * u64::BITS as usize
+            }
+
+            #[inline(always)]
+            pub(crate) fn as_slice(&self) -> &[$bits] {
+                &self.bits
+            }
+
+            #[inline]
+            pub(crate) fn iter(&self) -> impl Iterator<Item = u64> + '_ {
+                self.bits.iter().map(Self::fetch)
+            }
+
+            #[inline(always)]
+            pub(crate) fn check(&self, index: usize, hash: u64) -> bool {
+                let bit = 1u64 << (hash & BIT_MASK);
+                Self::fetch(&self.bits[index]) & bit > 0
+            }
+        }
+
+        impl FromIterator<u64> for $name {
+            fn from_iter<I: IntoIterator<Item = u64>>(iter: I) -> Self {
+                let mut bits = iter.into_iter().map(Self::new).collect::<Vec<_>>();
+                bits.shrink_to_fit();
+                Self { bits: bits.into() }
+            }
+        }
+
+        impl PartialEq for $name {
+            fn eq(&self, other: &Self) -> bool {
+                if self.len() != other.len() {
+                    return false;
+                }
+                core::iter::zip(self.iter(), other.iter()).all(|(l, r)| l == r)
+            }
+        }
+        impl Eq for $name {}
+    };
+}
+
+impl_bitvec!(BitVec, u64);
+impl_bitvec!(AtomicBitVec, AtomicU64);
+
+impl BitVec {
+    #[inline(always)]
+    fn new(x: u64) -> u64 {
+        x
     }
 
     #[inline(always)]
-    pub(crate) fn as_slice(&self) -> &[BitStorage] {
-        &self.bits
+    fn fetch(x: &u64) -> u64 {
+        *x
     }
 
-    #[inline]
-    pub(crate) fn iter(&self) -> impl Iterator<Item = u64> + '_ {
-        self.bits.iter().map(fetch)
-    }
-
-    #[inline(always)]
-    pub(crate) fn check(&self, index: usize, hash: u64) -> bool {
-        let bit = 1u64 << (hash & BIT_MASK);
-        fetch(&self.bits[index]) & bit > 0
-    }
-
-    #[cfg(not(feature = "atomic"))]
     #[inline(always)]
     pub(crate) fn set(&mut self, index: usize, hash: u64) -> bool {
         let bit = 1u64 << (hash & BIT_MASK);
-        let previously_contained = fetch(&self.bits[index]) & bit > 0;
+        let previously_contained = self.bits[index] & bit > 0;
         self.bits[index] |= bit;
         previously_contained
     }
 
-    #[cfg(feature = "atomic")]
-    #[inline(always)]
-    pub(crate) fn set(&self, index: usize, hash: u64) -> bool {
-        let bit = 1u64 << (hash & BIT_MASK);
-        let previously_contained = fetch(&self.bits[index]) & bit > 0;
-        self.bits[index].fetch_or(bit, Relaxed);
-        previously_contained
-    }
-
-    #[cfg(feature = "atomic")]
-    #[inline]
-    pub(crate) fn clear(&self) {
-        for i in 0..self.len() {
-            self.bits[i].store(0, Relaxed);
-        }
-    }
-
-    #[cfg(not(feature = "atomic"))]
     #[inline]
     pub(crate) fn clear(&mut self) {
         for i in 0..self.len() {
@@ -82,63 +104,79 @@ impl BlockedBitVec {
     }
 }
 
-impl FromIterator<u64> for BlockedBitVec {
-    fn from_iter<I: IntoIterator<Item = u64>>(iter: I) -> Self {
-        let mut bits = iter.into_iter().map(new).collect::<Vec<_>>();
-        bits.shrink_to_fit();
-        Self { bits: bits.into() }
+impl AtomicBitVec {
+    #[inline(always)]
+    fn new(x: u64) -> AtomicU64 {
+        AtomicU64::new(x)
     }
-}
 
-impl PartialEq for BlockedBitVec {
-    fn eq(&self, other: &Self) -> bool {
-        if self.len() != other.len() {
-            return false;
+    #[inline(always)]
+    fn fetch(x: &AtomicU64) -> u64 {
+        x.load(Relaxed)
+    }
+
+    #[inline(always)]
+    pub(crate) fn set(&self, index: usize, hash: u64) -> bool {
+        let bit = 1u64 << (hash & BIT_MASK);
+        let previously_contained = self.bits[index].load(Relaxed) & bit > 0;
+        self.bits[index].fetch_or(bit, Relaxed);
+        previously_contained
+    }
+
+    #[inline]
+    pub(crate) fn clear(&self) {
+        for i in 0..self.len() {
+            self.bits[i].store(0, Relaxed);
         }
-        core::iter::zip(self.iter(), other.iter()).all(|(l, r)| l == r)
     }
 }
-impl Eq for BlockedBitVec {}
 
-#[cfg(feature = "atomic")]
-impl Clone for BlockedBitVec {
+impl Clone for AtomicBitVec {
     fn clone(&self) -> Self {
         self.iter().collect()
     }
 }
 
-#[allow(unused_mut)]
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use core::iter::repeat;
-    use rand::Rng;
+macro_rules! impl_tests {
+    ($modname:ident, $name:ident) => {
+        #[allow(unused_mut)]
+        #[cfg(not(feature = "loom"))]
+        #[cfg(test)]
+        mod $modname {
+            use super::*;
+            use core::iter::repeat;
+            use rand::Rng;
 
-    #[test]
-    fn test_to_from_vec() {
-        let size = 42;
-        let b: BlockedBitVec = repeat(0).take(size).collect();
-        assert_eq!(b.num_bits(), b.len() * 64);
-        assert!(size <= b.len());
-        assert!((size + 64) > b.len());
-    }
-
-    #[test]
-    fn test_only_random_inserts_are_contained() {
-        let mut vec: BlockedBitVec = repeat(0).take(80).collect();
-        let mut control = Vec::with_capacity(1000);
-        let mut rng = rand::rng();
-
-        for _ in 0..1000 {
-            let block_index = rng.random_range(0..vec.num_bits() / 64);
-            let bit_index = rng.random_range(0..64);
-
-            if !control.contains(&(block_index, bit_index)) {
-                assert!(!vec.check(block_index, bit_index));
+            #[test]
+            fn test_to_from_vec() {
+                let size = 42;
+                let b: BitVec = repeat(0).take(size).collect();
+                assert_eq!(b.num_bits(), b.len() * 64);
+                assert!(size <= b.len());
+                assert!((size + 64) > b.len());
             }
-            control.push((block_index, bit_index));
-            vec.set(block_index, bit_index);
-            assert!(vec.check(block_index, bit_index));
+
+            #[test]
+            fn test_only_random_inserts_are_contained() {
+                let mut vec: BitVec = repeat(0).take(80).collect();
+                let mut control = Vec::with_capacity(1000);
+                let mut rng = rand::rng();
+
+                for _ in 0..1000 {
+                    let block_index = rng.random_range(0..vec.num_bits() / 64);
+                    let bit_index = rng.random_range(0..64);
+
+                    if !control.contains(&(block_index, bit_index)) {
+                        assert!(!vec.check(block_index, bit_index));
+                    }
+                    control.push((block_index, bit_index));
+                    vec.set(block_index, bit_index);
+                    assert!(vec.check(block_index, bit_index));
+                }
+            }
         }
-    }
+    };
 }
+
+impl_tests!(non_atomic, BitVec);
+impl_tests!(atomic, AtomicBitVec);
--- src/builder.rs
+++ src/builder.rs
@@ -1,253 +1,267 @@
-use crate::{BloomFilter, BuildHasher, DefaultHasher};
+use crate::{AtomicBloomFilter, BloomFilter, BuildHasher, DefaultHasher};
 use alloc::vec::Vec;
 use core::{f64::consts::LN_2, hash::Hash};
 
-/// A Bloom filter builder with an immutable number of bits.
-///
-/// This type can be used to construct an instance of [`BloomFilter`] via the builder pattern.
-///
-/// # Examples
-/// ```
-/// use fastbloom::BloomFilter;
-///
-/// let builder = BloomFilter::with_num_bits(1024);
-/// let builder = BloomFilter::from_vec(vec![0; 8]);
-/// ```
-#[derive(Debug, Clone)]
-pub struct BuilderWithBits<S = DefaultHasher> {
-    pub(crate) data: Vec<u64>,
-    pub(crate) hasher: S,
+macro_rules! builder_with_bits {
+    ($name:ident, $bloom:ident) => {
+        /// A Bloom filter builder with an immutable number of bits.
+        ///
+        /// This type can be used to construct an instance of [`BloomFilter`] via the builder pattern.
+        ///
+        /// # Examples
+        /// ```
+        /// use fastbloom::BloomFilter;
+        ///
+        /// let builder = BloomFilter::with_num_bits(1024);
+        /// let builder = BloomFilter::from_vec(vec![0; 8]);
+        /// ```
+        #[derive(Debug, Clone)]
+        pub struct $name<S = DefaultHasher> {
+            pub(crate) data: Vec<u64>,
+            pub(crate) hasher: S,
+        }
+
+        impl<S: BuildHasher> PartialEq for $name<S> {
+            fn eq(&self, other: &Self) -> bool {
+                self.data == other.data
+            }
+        }
+        impl<S: BuildHasher> Eq for $name<S> {}
+
+        impl $name {
+            /// Sets the seed for this builder. The later constructed [`BloomFilter`]
+            /// will use this seed when hashing items.
+            ///
+            /// # Examples
+            ///
+            /// ```
+            /// use fastbloom::BloomFilter;
+            ///
+            /// let bloom = BloomFilter::with_num_bits(1024).seed(&1).hashes(4);
+            /// ```
+            pub fn seed(mut self, seed: &u128) -> Self {
+                self.hasher = DefaultHasher::seeded(&seed.to_be_bytes());
+                self
+            }
+        }
+
+        impl<S: BuildHasher> $name<S> {
+            /// Sets the hasher for this builder. The later constructed [`BloomFilter`] will use
+            /// this hasher when inserting and checking items.
+            ///
+            /// # Examples
+            ///
+            /// ```
+            /// use fastbloom::BloomFilter;
+            /// use ahash::RandomState;
+            ///
+            /// let bloom = BloomFilter::with_num_bits(1024).hasher(RandomState::default()).hashes(4);
+            /// ```
+            pub fn hasher<H: BuildHasher>(self, hasher: H) -> $name<H> {
+                $name::<H> {
+                    data: self.data,
+                    hasher,
+                }
+            }
+
+            /// "Consumes" this builder, using the provided `num_hashes` to return an
+            /// empty [`BloomFilter`].
+            ///
+            /// # Examples
+            /// ```
+            /// use fastbloom::BloomFilter;
+            ///
+            /// let bloom = BloomFilter::with_num_bits(1024).hashes(4);
+            /// ```
+            pub fn hashes(self, num_hashes: u32) -> $bloom<S> {
+                $bloom {
+                    bits: self.data.into_iter().collect(),
+                    num_hashes,
+                    hasher: self.hasher,
+                }
+            }
+
+            /// "Consumes" this builder, using the provided `expected_num_items` to return an
+            /// empty [`BloomFilter`]. The number of hashes is optimized based on `expected_num_items`
+            /// to maximize Bloom filter accuracy (minimize false positives chance on [`BloomFilter::contains`]).
+            /// More or less than `expected_num_items` may be inserted into [`BloomFilter`].
+            ///
+            /// # Examples
+            ///
+            /// ```
+            /// use fastbloom::BloomFilter;
+            ///
+            /// let bloom = BloomFilter::with_num_bits(1024).expected_items(500);
+            /// ```
+            pub fn expected_items(self, expected_num_items: usize) -> $bloom<S> {
+                let num_hashes = optimal_hashes_f(self.data.len(), expected_num_items);
+                self.hashes(num_hashes as u32)
+            }
+
+            /// "Consumes" this builder and constructs a [`BloomFilter`] containing
+            /// all values in `items`. Like [`BuilderWithBits::expected_items`], the number of hashes per item
+            /// is optimized based on `items.len()` to maximize Bloom filter accuracy
+            /// (minimize false positives chance on [`BloomFilter::contains`]).
+            ///
+            /// # Examples
+            ///
+            /// ```
+            /// use fastbloom::BloomFilter;
+            ///
+            /// let bloom = BloomFilter::with_num_bits(1024).items([1, 2, 3]);
+            /// ```
+            pub fn items<I: IntoIterator<IntoIter = impl ExactSizeIterator<Item = impl Hash>>>(
+                self,
+                items: I,
+            ) -> $bloom<S> {
+                let into_iter = items.into_iter();
+                let mut filter = self.expected_items(into_iter.len());
+                filter.extend(into_iter);
+                filter
+            }
+        }
+    };
 }
 
-impl<S: BuildHasher> PartialEq for BuilderWithBits<S> {
-    fn eq(&self, other: &Self) -> bool {
-        self.data == other.data
+builder_with_bits!(BuilderWithBits, BloomFilter);
+builder_with_bits!(AtomicBuilderWithBits, AtomicBloomFilter);
+
+macro_rules! builder_with_fp {
+    ($name:ident, $bloom:ident) => {
+        /// A Bloom filter builder with an immutable false positive rate.
+        ///
+        /// This type can be used to construct an instance of [`BloomFilter`] via the builder pattern.
+        ///
+        /// # Examples
+        ///
+        /// ```
+        /// use fastbloom::BloomFilter;
+        ///
+        /// let builder = BloomFilter::with_false_pos(0.01);
+        /// ```
+        #[derive(Debug, Clone)]
+        pub struct $name<S = DefaultHasher> {
+            pub(crate) desired_fp_rate: f64,
+            pub(crate) hasher: S,
+        }
+
+        impl<S: BuildHasher> PartialEq for $name<S> {
+            fn eq(&self, other: &Self) -> bool {
+                self.desired_fp_rate == other.desired_fp_rate
+            }
+        }
+        impl<S: BuildHasher> Eq for $name<S> {}
+
+        impl $name {
+            /// Sets the seed for this builder. The later constructed [`BloomFilter`]
+            /// will use this seed when hashing items.
+            ///
+            /// # Examples
+            ///
+            /// ```
+            /// use fastbloom::BloomFilter;
+            ///
+            /// let bloom = BloomFilter::with_false_pos(0.001).seed(&1).expected_items(100);
+            /// ```
+            pub fn seed(mut self, seed: &u128) -> Self {
+                self.hasher = DefaultHasher::seeded(&seed.to_be_bytes());
+                self
+            }
+        }
+
+        impl<S: BuildHasher> $name<S> {
+            /// Sets the hasher for this builder. The later constructed [`BloomFilter`] will use
+            /// this hasher when inserting and checking items.
+            ///
+            /// # Examples
+            ///
+            /// ```
+            /// use fastbloom::BloomFilter;
+            /// use ahash::RandomState;
+            ///
+            /// let bloom = BloomFilter::with_false_pos(0.001).hasher(RandomState::default()).expected_items(100);
+            /// ```
+            pub fn hasher<H: BuildHasher>(self, hasher: H) -> $name<H> {
+                $name::<H> {
+                    desired_fp_rate: self.desired_fp_rate,
+                    hasher,
+                }
+            }
+
+            /// "Consumes" this builder, using the provided `expected_num_items` to return an
+            /// empty [`BloomFilter`]. The number of hashes and underlying memory is optimized based on `expected_num_items`
+            /// to meet the desired false positive rate.
+            /// More or less than `expected_num_items` may be inserted into [`BloomFilter`].
+            ///
+            /// # Examples
+            ///
+            /// ```
+            /// use fastbloom::BloomFilter;
+            ///
+            /// let bloom = BloomFilter::with_false_pos(0.001).expected_items(500);
+            /// ```
+            pub fn expected_items(self, expected_num_items: usize) -> $bloom<S> {
+                let num_bits = optimal_size(expected_num_items as f64, self.desired_fp_rate);
+                $bloom::new_builder(num_bits)
+                    .hasher(self.hasher)
+                    .expected_items(expected_num_items)
+            }
+
+            /// "Consumes" this builder and constructs a [`BloomFilter`] containing
+            /// all values in `items`. Like [`BuilderWithFalsePositiveRate::expected_items`], the number of hashes per item
+            /// and underlying memory is optimized based on `items.len()` to meet the desired false positive rate.
+            ///
+            /// # Examples
+            ///
+            /// ```
+            /// use fastbloom::BloomFilter;
+            ///
+            /// let bloom = BloomFilter::with_false_pos(0.001).items([1, 2, 3]);
+            /// ```
+            pub fn items<I: IntoIterator<IntoIter = impl ExactSizeIterator<Item = impl Hash>>>(
+                self,
+                items: I,
+            ) -> $bloom<S> {
+                let into_iter = items.into_iter();
+                let mut filter = self.expected_items(into_iter.len());
+                filter.extend(into_iter);
+                filter
+            }
+        }
+    };
+}
+
+builder_with_fp!(BuilderWithFalsePositiveRate, BloomFilter);
+builder_with_fp!(AtomicBuilderWithFalsePositiveRate, AtomicBloomFilter);
+
+/// The optimal number of hashes to perform for an item given the expected number of items in the bloom filter.
+/// Proof under "False Positives Analysis": <https://brilliant.org/wiki/bloom-filter/>.
+#[inline]
+fn optimal_hashes_f(len: usize, expected_num_items: usize) -> f64 {
+    let load = expected_num_items as f64 / len as f64;
+    let x = 64.0f64 / load;
+    let hashes_per_u64 = x * f64::ln(2.0f64);
+
+    let max_hashes = hashes_for_bits(32);
+    if hashes_per_u64 > max_hashes {
+        max_hashes
+    } else if hashes_per_u64 < 1.0 {
+        1.0
+    } else {
+        hashes_per_u64
     }
 }
-impl<S: BuildHasher> Eq for BuilderWithBits<S> {}
 
-impl BuilderWithBits {
-    /// Sets the seed for this builder. The later constructed [`BloomFilter`]
-    /// will use this seed when hashing items.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use fastbloom::BloomFilter;
-    ///
-    /// let bloom = BloomFilter::with_num_bits(1024).seed(&1).hashes(4);
-    /// ```
-    pub fn seed(mut self, seed: &u128) -> Self {
-        self.hasher = DefaultHasher::seeded(&seed.to_be_bytes());
-        self
-    }
-}
-
-impl<S: BuildHasher> BuilderWithBits<S> {
-    /// Sets the hasher for this builder. The later constructed [`BloomFilter`] will use
-    /// this hasher when inserting and checking items.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use fastbloom::BloomFilter;
-    /// use ahash::RandomState;
-    ///
-    /// let bloom = BloomFilter::with_num_bits(1024).hasher(RandomState::default()).hashes(4);
-    /// ```
-    pub fn hasher<H: BuildHasher>(self, hasher: H) -> BuilderWithBits<H> {
-        BuilderWithBits::<H> {
-            data: self.data,
-            hasher,
-        }
-    }
-
-    /// "Consumes" this builder, using the provided `num_hashes` to return an
-    /// empty [`BloomFilter`].
-    ///
-    /// # Examples
-    /// ```
-    /// use fastbloom::BloomFilter;
-    ///
-    /// let bloom = BloomFilter::with_num_bits(1024).hashes(4);
-    /// ```
-    pub fn hashes(self, num_hashes: u32) -> BloomFilter<S> {
-        BloomFilter {
-            bits: self.data.into_iter().collect(),
-            num_hashes,
-            hasher: self.hasher,
-        }
-    }
-
-    /// "Consumes" this builder, using the provided `expected_num_items` to return an
-    /// empty [`BloomFilter`]. The number of hashes is optimized based on `expected_num_items`
-    /// to maximize Bloom filter accuracy (minimize false positives chance on [`BloomFilter::contains`]).
-    /// More or less than `expected_num_items` may be inserted into [`BloomFilter`].
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use fastbloom::BloomFilter;
-    ///
-    /// let bloom = BloomFilter::with_num_bits(1024).expected_items(500);
-    /// ```
-    pub fn expected_items(self, expected_num_items: usize) -> BloomFilter<S> {
-        let num_hashes = Self::optimal_hashes_f(self.data.len(), expected_num_items);
-        self.hashes(num_hashes as u32)
-    }
-
-    /// The optimal number of hashes to perform for an item given the expected number of items in the bloom filter.
-    /// Proof under "False Positives Analysis": <https://brilliant.org/wiki/bloom-filter/>.
-    #[inline]
-    fn optimal_hashes_f(len: usize, expected_num_items: usize) -> f64 {
-        let load = expected_num_items as f64 / len as f64;
-        let x = 64.0f64 / load;
-        let hashes_per_u64 = x * f64::ln(2.0f64);
-
-        let max_hashes = Self::hashes_for_bits(32);
-        if hashes_per_u64 > max_hashes {
-            max_hashes
-        } else if hashes_per_u64 < 1.0 {
-            1.0
-        } else {
-            hashes_per_u64
-        }
-    }
-
-    #[inline]
-    pub(crate) fn hashes_for_bits(target_bits_per_u64_per_item: u64) -> f64 {
-        f64::ln(-(((target_bits_per_u64_per_item as f64) / 64.0f64) - 1.0f64))
-            / f64::ln(63.0f64 / 64.0f64)
-    }
-
-    /// "Consumes" this builder and constructs a [`BloomFilter`] containing
-    /// all values in `items`. Like [`BuilderWithBits::expected_items`], the number of hashes per item
-    /// is optimized based on `items.len()` to maximize Bloom filter accuracy
-    /// (minimize false positives chance on [`BloomFilter::contains`]).
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use fastbloom::BloomFilter;
-    ///
-    /// let bloom = BloomFilter::with_num_bits(1024).items([1, 2, 3]);
-    /// ```
-    pub fn items<I: IntoIterator<IntoIter = impl ExactSizeIterator<Item = impl Hash>>>(
-        self,
-        items: I,
-    ) -> BloomFilter<S> {
-        let into_iter = items.into_iter();
-        let mut filter = self.expected_items(into_iter.len());
-        filter.extend(into_iter);
-        filter
-    }
+#[inline]
+fn hashes_for_bits(target_bits_per_u64_per_item: u64) -> f64 {
+    f64::ln(-(((target_bits_per_u64_per_item as f64) / 64.0f64) - 1.0f64))
+        / f64::ln(63.0f64 / 64.0f64)
 }
 
 fn optimal_size(items_count: f64, fp_p: f64) -> usize {
     let log2_2 = LN_2 * LN_2;
     let result = 8 * ((items_count) * f64::ln(fp_p) / (-8.0 * log2_2)).ceil() as usize;
     core::cmp::max(result, 512)
-}
-
-/// A Bloom filter builder with an immutable false positive rate.
-///
-/// This type can be used to construct an instance of [`BloomFilter`] via the builder pattern.
-///
-/// # Examples
-///
-/// ```
-/// use fastbloom::BloomFilter;
-///
-/// let builder = BloomFilter::with_false_pos(0.01);
-/// ```
-#[derive(Debug, Clone)]
-pub struct BuilderWithFalsePositiveRate<S = DefaultHasher> {
-    pub(crate) desired_fp_rate: f64,
-    pub(crate) hasher: S,
-}
-
-impl<S: BuildHasher> PartialEq for BuilderWithFalsePositiveRate<S> {
-    fn eq(&self, other: &Self) -> bool {
-        self.desired_fp_rate == other.desired_fp_rate
-    }
-}
-impl<S: BuildHasher> Eq for BuilderWithFalsePositiveRate<S> {}
-
-impl BuilderWithFalsePositiveRate {
-    /// Sets the seed for this builder. The later constructed [`BloomFilter`]
-    /// will use this seed when hashing items.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use fastbloom::BloomFilter;
-    ///
-    /// let bloom = BloomFilter::with_false_pos(0.001).seed(&1).expected_items(100);
-    /// ```
-    pub fn seed(mut self, seed: &u128) -> Self {
-        self.hasher = DefaultHasher::seeded(&seed.to_be_bytes());
-        self
-    }
-}
-
-impl<S: BuildHasher> BuilderWithFalsePositiveRate<S> {
-    /// Sets the hasher for this builder. The later constructed [`BloomFilter`] will use
-    /// this hasher when inserting and checking items.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use fastbloom::BloomFilter;
-    /// use ahash::RandomState;
-    ///
-    /// let bloom = BloomFilter::with_false_pos(0.001).hasher(RandomState::default()).expected_items(100);
-    /// ```
-    pub fn hasher<H: BuildHasher>(self, hasher: H) -> BuilderWithFalsePositiveRate<H> {
-        BuilderWithFalsePositiveRate::<H> {
-            desired_fp_rate: self.desired_fp_rate,
-            hasher,
-        }
-    }
-
-    /// "Consumes" this builder, using the provided `expected_num_items` to return an
-    /// empty [`BloomFilter`]. The number of hashes and underlying memory is optimized based on `expected_num_items`
-    /// to meet the desired false positive rate.
-    /// More or less than `expected_num_items` may be inserted into [`BloomFilter`].
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use fastbloom::BloomFilter;
-    ///
-    /// let bloom = BloomFilter::with_false_pos(0.001).expected_items(500);
-    /// ```
-    pub fn expected_items(self, expected_num_items: usize) -> BloomFilter<S> {
-        let num_bits = optimal_size(expected_num_items as f64, self.desired_fp_rate);
-        BloomFilter::new_builder(num_bits)
-            .hasher(self.hasher)
-            .expected_items(expected_num_items)
-    }
-
-    /// "Consumes" this builder and constructs a [`BloomFilter`] containing
-    /// all values in `items`. Like [`BuilderWithFalsePositiveRate::expected_items`], the number of hashes per item
-    /// and underlying memory is optimized based on `items.len()` to meet the desired false positive rate.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use fastbloom::BloomFilter;
-    ///
-    /// let bloom = BloomFilter::with_false_pos(0.001).items([1, 2, 3]);
-    /// ```
-    pub fn items<I: IntoIterator<IntoIter = impl ExactSizeIterator<Item = impl Hash>>>(
-        self,
-        items: I,
-    ) -> BloomFilter<S> {
-        let into_iter = items.into_iter();
-        let mut filter = self.expected_items(into_iter.len());
-        filter.extend(into_iter);
-        filter
-    }
 }
 
 #[cfg(test)]
